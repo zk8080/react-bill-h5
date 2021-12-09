460758f063a2041c8866161e130aa009d1deed0f
zk8080 <zhangkang8080@163.com>
2021-12-09 22:48:43 +0800

    wip: 账单金额

--- src/components/PopupAddBill/index.tsx
+++ src/components/PopupAddBill/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Icon, Popup } from 'zarm';
+import { Icon, Keyboard, Popup } from 'zarm';
 import type { PopupProps } from 'zarm/types/popup/Popup';
 import classNames from 'classnames';
 import style from './style.module.less';
@@ -18,6 +18,7 @@ const PopupAddBill = (props: IProps) => {
   const [payType, setPayType] = useState<PayType>('expense'); // 支出或收入类型
   const [addDate, setAddDate] = useState<string>(dayjs().format('MM-DD')); // 新增日期
   const [dateVisible, setDateVisible] = useState<boolean>(false); // 日期弹窗
+  const [amount, setAmount] = useState<string>('');
 
   // 选择类型
   const changeType = (type: PayType) => {
@@ -32,6 +33,30 @@ const PopupAddBill = (props: IProps) => {
   // 选择日期
   const handleDateChange = (date: string) => {
     setAddDate(date);
+  }
+
+  // 监听输入框改变值
+  const handleMoney = (value?: string) => {
+    value = String(value)
+    // 点击是删除按钮时
+    if (value == 'delete') {
+      let _amount = amount.slice(0, amount.length - 1)
+      setAmount(_amount)
+      return
+    }
+
+    // 点击确认按钮时
+    if (value == 'ok') {
+      // 这里后续将处理添加账单逻辑
+      return
+    }
+
+    // 当输入的值为 '.' 且 已经存在 '.'，则不让其继续字符串相加。
+    if (value == '.' && amount.includes('.')) return
+    // 小数点后保留两位，当超过两位时，不让其字符串继续相加。
+    if (value != '.' && amount.includes('.') && amount && amount.split('.')[1].length >= 2) return
+    // amount += value
+    setAmount(amount + value)
   }
 
   return <Popup
@@ -57,6 +82,11 @@ const PopupAddBill = (props: IProps) => {
           onClick={toggleDate}
         >{addDate} <Icon className={style.arrow} type="arrow-bottom" /></div>
       </div>
+      <div className={style.money}>
+        <span className={style.sufix}>¥</span>
+        <span className={classNames(style.amount, style.animation)}>{amount}</span>
+      </div>
+      <Keyboard type="price" onKeyClick={handleMoney} />
     </div>
     <PopupDate
       mode="date"
